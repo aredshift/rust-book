5b07627ea7c0f889cca92ff9c84d7be148e1b3f0
aredshift <123187978+aredshift@users.noreply.github.com>
2024-04-11 14:50:02 -0500

    remove binary, add **/target to gitignore

--- ch5-structs/rectangles/target/debug/deps/rectangles-eeac19cc862699ba.d
+++
@@ -1,5 +0,0 @@
-/home/shane/dev/rust-book/rectangles/target/debug/deps/rectangles-eeac19cc862699ba: src/main.rs
-
-/home/shane/dev/rust-book/rectangles/target/debug/deps/rectangles-eeac19cc862699ba.d: src/main.rs
-
-src/main.rs:
--- ch5-structs/rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/home/shane/dev/rust-book/ch5-structs/rectangles/target/debug/rectangles: /home/shane/dev/rust-book/ch5-structs/rectangles/src/main.rs

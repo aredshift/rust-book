5502feeffe09af8cec83edaf74a518c2350b82ba
aredshift <123187978+aredshift@users.noreply.github.com>
2024-04-11 15:20:08 -0500

    finish structs chapter

--- ch5-structs/rectangles/src/main.rs
+++ ch5-structs/rectangles/src/main.rs
@@ -50,10 +50,49 @@
 // }
 
 // v4 - Structs refactor with nifty printings
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     println!("The area of rectangle {:#?} is {}.", &rect1, area(&rect1));
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// v5 - Structs and method impl
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Self) -> bool {
+        // Can be &Self or &Rectangle
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -63,9 +102,20 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of rectangle {:#?} is {}.", &rect1, area(&rect1));
-}
+    println!("The area of rectangle {:#?} is {}.", &rect1, rect1.area());
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 1,
+        height: 1,
+    };
+
+    println!(
+        "It is {} that rect1 can hold rect2.",
+        rect1.can_hold(&rect2)
+    );
+
+    // "associated function" used as a constructor
+    let square1 = Rectangle::square(5);
+
+    println!("Check out this square! {:#?}", &square1)
 }
